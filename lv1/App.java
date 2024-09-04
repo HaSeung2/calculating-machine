a77edaff24e40f9fc6e6f4e3977325bbe696bd07
하승 <dlgktmd0@naver.com>
2024-09-04 17:06:15 +0900

    try ~ catch문으로 예  정수가 아닌 다른 값 입력했을 때를 대비한 예외처리

--- lv1/App.java
+++ lv1/App.java
@@ -1,25 +1,45 @@
 package calculatingMachine.lv1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         // exit를 입력받으면 flag를 false로 바꿔 while문 종료.
         boolean flag = true;
         // 연산 결과를 저장하기 위한 공간
         int result = 0;
+        int firstNumber = 0;
+        String operation = "";
+        int secondNumber = 0;
+
         while (flag) {
-            System.out.print("숫자 입력 : ");
-            int firstNumber = sc.nextInt();
-            // nextLine을 쓸때 위에 nextLine이 아닌 다른 입력이 있으면 엔터키를 하나 더 먹는 이슈가 있어서 방패막으로 의미없는 line써줌.
-            sc.nextLine();
-            System.out.print("사칙연산 기호 입력(+,-,*,/) : ");
-            String operation = sc.nextLine();
-            System.out.print("숫자 입력 : ");
-            int secondNumber = sc.nextInt();
-            
+                // 정수가 아닌 다른 타입의 문자를 입력을 방지하기위해 try ~ catch 문을 이용
+                try{
+                    System.out.print("숫자 입력 : ");
+                    firstNumber = sc.nextInt();
+                    // nextLine을 쓸때 위에 nextLine이 아닌 다른 입력이 있으면 엔터키를 하나 더 먹는 이슈가 있어서 방패막으로 의미없는 line써줌.
+                    sc.nextLine();
+                }
+                // 정수가 아닌 다른 타입의 문자를 입력했다면 아래에 문구를 출력 후 실행 중지.
+                catch (InputMismatchException e) {
+                    System.out.println("정수만 입력해주세요 !");
+                    System.out.println("다시 실행해주세요 ~ ");
+                    break;
+                }
+                System.out.print("사칙연산 기호 입력(+,-,*,/) : ");
+                operation = sc.nextLine();
+                try{
+                    System.out.print("숫자 입력 : ");
+                    secondNumber = sc.nextInt();
+                }
+                // 정수가 아닌 다른 타입의 문자를 입력했다면 아래에 문구를 출력 후 실행 중지.
+                catch (InputMismatchException e) {
+                    System.out.println("정수만 입력해주세요 !");
+                    System.out.println("다시 실행해주세요 ~ ");
+                    break;
+                }
             switch (operation) {
                 case "+":
                     result = firstNumber + secondNumber;
@@ -36,7 +56,7 @@ public class App {
                 // +,-,*,/ 가 아닌 다른 것을 입력하면 다시 입력해주세요라는 출력문과 함께 
                 // switch문 반복
                 default:
-                    System.out.println("다시 입력해주세요");
+                    System.out.println("연산기호를 다시 확인해주세요 !");
                     continue;
             }
             System.out.println("결과 " + result);
--- lv2/App.java
+++ lv2/App.java
@@ -1,5 +1,6 @@
 package calculatingMachine.lv2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -10,18 +11,34 @@ public class App {
         boolean flag = true;
         // 연산 결과를 저장하기 위한 공간
         int result = 0;
-
+        int firstNumber = 0;
+        int secondNumber = 0;
         while (flag) {
-            System.out.print("숫자 입력 : ");
-            int firstNumber = sc.nextInt();
-            // nextLine을 쓸때 위에 nextLine이 아닌 다른 입력이 있으면 엔터키를 하나 더 먹는 이슈가 있어서 방패막으로 의미없는 line써줌.
-            sc.nextLine();
+            try {
+                System.out.print("숫자 입력 : ");
+                firstNumber = sc.nextInt();
+                // nextLine을 쓸때 위에 nextLine이 아닌 다른 입력이 있으면 엔터키를 하나 더 먹는 이슈가 있어서 방패막으로 의미없는 line써줌.
+                sc.nextLine();
+
+            } catch (InputMismatchException e) {
+                System.out.println("정수만 입력해주세요 !");
+                System.out.println("다시 실행해주세요 ~ ");
+                break;
+            }
             System.out.print("사칙연산 기호 입력(+,-,*,/) : ");
             String operation = sc.nextLine();
-            System.out.print("숫자 입력 : ");
-            int secondNumber = sc.nextInt();
 
-            if(operation.contains("+") || operation.contains("-") || operation.contains("*") || operation.contains("/")) {
+            try {
+                System.out.print("숫자 입력 : ");
+                secondNumber = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("정수만 입력해주세요 !");
+                System.out.println("다시 실행해주세요 ~ ");
+                break;
+            }
+
+            //사칙 연산 기호를 제대로 썼다면 메서드 실행.
+            if (operation.contains("+") || operation.contains("-") || operation.contains("*") || operation.contains("/")) {
                 calculator.calculator(operation, firstNumber, secondNumber);
                 System.out.print("더 계산하시겠습니까 ? (exit 입력시 종료) : ");
                 String answer = sc.next();
@@ -30,29 +47,35 @@ public class App {
                     flag = false;
                 }
                 // 그게 아니라면 while문 반복
-                else{
+                else {
                     flag = true;
                 }
             }
-            else{
+            // 사칙 연산 기호를 알맞게 입력하지 않았다면 다시 처음으로 돌아가 입력 다시
+            else {
                 System.out.println("사칙연산 기호 다시 입력해주세요.");
             }
         }
-        // 리스트의 저장된 값 모두 가져옴
-        calculator.getArrNumbersAll();
+        // 가져올 수 있는 데이터가 없을 때 예외 처리
+        try {
+            // 리스트의 저장된 값 모두 가져옴
+            calculator.getArrNumbersAll();
 
-        // 보내준 idxNumber에 해당하는 값 가져옴
-        int arrGetNum = calculator.getArrNumber(0);
-        System.out.println(arrGetNum);
+            // 보내준 idxNumber에 해당하는 값 가져옴
+            int arrGetNum = calculator.getArrNumber(0);
+            System.out.println(arrGetNum);
 
-        // 리스트의 저장된 데이터 수정
-        calculator.setArrNumbers(0,30);
-        System.out.println();
-        calculator.getArrNumbersAll();
+            // 리스트의 저장된 데이터 수정
+            calculator.setArrNumbers(0, 30);
+            System.out.println();
+            calculator.getArrNumbersAll();
 
-        // 리스트에 가장 먼저 담긴 데이터 삭제
-        calculator.removeArrNumber();
-        System.out.println();
-        calculator.getArrNumbersAll();
+            // 리스트에 가장 먼저 담긴 데이터 삭제
+            calculator.removeArrNumber();
+            System.out.println();
+            calculator.getArrNumbersAll();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("충분한 데이터가 없습니다.");
+        }
     }
 }
